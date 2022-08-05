ff9fe02e751aecbd40464de3909915caef4f53c4
taeyangcode <coreymostero@gmail.com>
2022-08-05 06:41:53 -0700

    Removed unnecessary interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import { useState } from "react";
 import SearchBar from "./SearchBar";
 import YoutubePlayer from "./YoutubePlayer";
 
-export interface Ref {
-	changeHeaderContent: (songUrl: string) => void;
-}
-
 function App() {
 	const [songUrls, setSongUrl] = useState([""]);
 
